f62f3508a2443e86aefb184087b295318768fef0
lathifinch <lathifinch@gmail.com>
2019-11-18 12:21:05 +0700

    Add: transfer opo cash between user

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ const Route = express.Router();
 
 // import required files
 const user = require('./routes/user');
+const balance = require('./routes/balance')
 
 console.log('index'); // where I am
 
 // create routes
 Route
+    .use('/api/v1/balance', balance)
     .use('/api/v1/user', user);
 
 module.exports = Route;
---
+++ src/routes/balance.js
@@ -0,0 +1,18 @@
+// import required dependencies
+const express = require('express');
+const Route = express.Router();
+const {check} = require('express-validator');
+
+// const { check, validationResult } = require('express-validator');
+
+// import required files
+const balanceController = require('../controllers/balance');
+// const isAuthHelper = require('../helpers/isAuth');
+
+console.log('route'); // where I am
+
+Route
+		.patch('/transfer/:userId', [check('phoneTo').isMobilePhone(), check('nominal').isNumeric()],
+			balanceController.transfer);
+
+module.exports = Route;
